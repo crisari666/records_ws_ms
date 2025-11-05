0f15576d9c65b966d4da30ec83d76764bf91039a
KDEV999 <cristhiangrojas@gmail.com>
2025-11-05 16:47:05 -0500

    Enhance WhatsApp Web service configuration by adding additional launch options
    - Added '--no-zygote' and '--single-process' flags to improve process management and stability during service initialization.

--- src/whatsapp-web/whatsapp-web.service.ts
+++ src/whatsapp-web/whatsapp-web.service.ts
@@ -328,7 +328,9 @@ export class WhatsappWebService implements OnModuleInit {
           '--no-sandbox',
           '--disable-setuid-sandbox',
           '--disable-dev-shm-usage',
-          '--disable-gpu'
+          '--disable-gpu',
+          '--no-zygote',
+          '--single-process',
         ],
       };
 
